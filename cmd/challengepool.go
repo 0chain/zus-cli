0d01ac2191c6c5491c5a8db5a6ab808a959a67ce
storybehind <arunmail12@gmail.com>
2024-10-03 14:56:14 +0530

    setup config

---
+++ cmd/challengepool.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/0chain/gosdk/zboxcore/sdk"
+	"github.com/0chain/zus-cli/util"
+	"github.com/spf13/cobra"
+)
+
+func printChallengePoolInfo(info *sdk.ChallengePoolInfo) {
+	var header = []string{
+		"BALANCE", "START", "EXPIRE", "FINIALIZED",
+	}
+	var data = [][]string{{
+		info.Balance.String(),
+		info.StartTime.ToTime().String(),
+		info.Expiration.ToTime().String(),
+		fmt.Sprint(info.Finalized),
+	}}
+	fmt.Println("POOL ID:", info.ID)
+	util.WriteTable(os.Stdout, header, []string{}, data)
+	fmt.Println()
+}
+
+// cpInfo information
+var cpInfo = &cobra.Command{
+	Use:   "cp-info",
+	Short: "Challenge pool information.",
+	Long:  `Challenge pool information.`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		var (
+			flags   = cmd.Flags()
+			allocID string
+			err     error
+		)
+
+		doJSON, _ := cmd.Flags().GetBool("json")
+
+		if !flags.Changed("allocation") {
+			log.Fatal("missing required 'allocation' flag")
+		}
+
+		if allocID, err = flags.GetString("allocation"); err != nil {
+			log.Fatalf("can't get 'allocation' flag: %v", err)
+		}
+
+		var info *sdk.ChallengePoolInfo
+		if info, err = sdk.GetChallengePoolInfo(allocID); err != nil {
+			log.Fatalf("Failed to get challenge pool info: %v", err)
+		}
+		if doJSON {
+			util.PrintJSON(info)
+		} else {
+			printChallengePoolInfo(info)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cpInfo)
+
+	cpInfo.PersistentFlags().String("allocation", "",
+		"allocation identifier, required")
+	cpInfo.Flags().Bool("json", false, "(default false) pass this option to print response as json data")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,46 @@
 package cmd
 
 import (
+	_ "embed"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/0chain/gosdk/core/conf"
+	"github.com/0chain/gosdk/core/logger"
+	"github.com/0chain/gosdk/core/zcncrypto"
+	"github.com/0chain/gosdk/zboxcore/blockchain"
+	"github.com/0chain/gosdk/zboxcore/sdk"
+	"github.com/0chain/gosdk/zcncore"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+// rootCmd flags
+var cfgFile string
+var networkFile string
+var walletFile string
+var walletClientID string
+var walletClientKey string
+var cDir string
+var nonce int64
+var txFee float64
+var bSilent bool
+
+// default configuration
+//
+//go:embed config.yaml
+var configStr string
+
+// wallet info
+var walletJSON string
+var clientWallet *zcncrypto.Wallet
+
+var cfg conf.Config
+var network conf.Network
 
 var rootCmd = &cobra.Command{
 	Use: "zus-cli",
@@ -15,9 +50,279 @@ var rootCmd = &cobra.Command{
 			Complete documentation is available at https://docs.zus.network/guides/zbox-cli`,
 }
 
+func init() {
+
+	InstallDLLs()
+
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yaml", "config file (default is config.yaml)")
+	rootCmd.PersistentFlags().StringVar(&networkFile, "network", "network.yaml", "network file to overwrite the network details (if required, default is network.yaml)")
+	rootCmd.PersistentFlags().StringVar(&walletFile, "wallet", "wallet.json", "wallet file (default is wallet.json)")
+	rootCmd.PersistentFlags().StringVar(&walletClientID, "wallet_client_id", "", "wallet client_id")
+	rootCmd.PersistentFlags().StringVar(&walletClientKey, "wallet_client_key", "", "wallet client_key")
+	rootCmd.PersistentFlags().Int64Var(&nonce, "withNonce", 0, "nonce that will be used in transaction (default is 0)")
+	rootCmd.PersistentFlags().StringVar(&cDir, "configDir", "", "configuration directory")
+	rootCmd.PersistentFlags().BoolVar(&bSilent, "silent", false, "(default false) Do not show interactive sdk logs (shown by default)")
+	rootCmd.PersistentFlags().Float64Var(&txFee, "fee", 0, "transaction fee for the given transaction (if unset, it will be set to blockchain min fee)")
+}
+
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+// returns full path of application's default configuration directory
+func GetAppConfigDir() (string, error) {
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	appConfigDir := userConfigDir + string(os.PathSeparator) + ".zcn"
+	return appConfigDir, nil
+}
+
+// loads and returns default configuration
+func LoadDefaultConfig() (conf.Config, error) {
+	v := viper.New()
+	v.SetConfigType("yaml")
+	err := v.ReadConfig(strings.NewReader(configStr))
+	if err != nil {
+		fmt.Println("error reading default config:", err)
+		return conf.Config{}, err
+	}
+	cfg, err := conf.LoadConfig(v)
+	if err != nil {
+		fmt.Println("error loading default config:", err)
+		return conf.Config{}, err
+	}
+	return cfg, nil
+}
+
+// returns full path of application configuration directory
+func GetConfigDir() (string, error) {
+	if len(cDir) != 0 {
+		return cDir, nil
+	}
+	appConfigDir, err := GetAppConfigDir()
+	if err != nil {
+		return "", err
+	}
+	return appConfigDir, nil
+}
+
+// loads and returns configuration
+func LoadConfig() (conf.Config, error) {
+	configDir, _ := GetConfigDir()
+	cfg, err := conf.LoadConfigFile(filepath.Join(configDir, cfgFile))
+	if err == nil {
+		return cfg, nil
+	}
+	fmt.Println("Can't read config:", err)
+	fmt.Println("using default config")
+	fmt.Printf("config: %v", configStr)
+	cfg, err = LoadDefaultConfig()
+	if err == nil {
+		return cfg, nil
+	}
+	return conf.Config{}, err
+}
+
+// loads and returns network configuration
+func LoadNetworkFile() (conf.Network, error) {
+	configDir, _ := GetConfigDir()
+	network, _ := conf.LoadNetworkFile(filepath.Join(configDir, networkFile))
+	return network, nil
+}
+
+// initilaize gosdk
+func initSDK() (err error) {
+	// syncing loggers
+	logger.SyncLoggers([]*logger.Logger{zcncore.GetLogger(), sdk.GetLogger()})
+
+	// set the log file
+	zcncore.SetLogFile("cmdlog.log", !bSilent)
+	sdk.SetLogFile("cmdlog.log", !bSilent)
+	// CHECK
+	sdk.SetMinSubmit(cfg.MinSubmit)
+
+	if network.IsValid() {
+		zcncore.SetNetwork(network.Miners, network.Sharders)
+		conf.InitChainNetwork(&conf.Network{
+			Miners:   network.Miners,
+			Sharders: network.Sharders,
+		})
+	}
+
+	err = zcncore.InitZCNSDK(cfg.BlockWorker, cfg.SignatureScheme,
+		zcncore.WithChainID(cfg.ChainID),
+		zcncore.WithMinSubmit(cfg.MinSubmit),
+		zcncore.WithMinConfirmation(cfg.MinConfirmation),
+		zcncore.WithConfirmationChainLength(cfg.ConfirmationChainLength))
+	if err != nil {
+		fmt.Println("Error initializing core SDK.", err)
+		return
+	}
+
+	return nil
+}
+
+// loads wallet and return
+func loadWallet() (*zcncrypto.Wallet, string, error) {
+	wallet := &zcncrypto.Wallet{}
+	if (len(walletClientID) > 0) && (len(walletClientKey) > 0) {
+		wallet.ClientID = walletClientID
+		wallet.ClientKey = walletClientKey
+		clientBytes, err := json.Marshal(wallet)
+		if err != nil {
+			err = fmt.Errorf("Invalid wallet data passed:" + walletClientID + " " + walletClientKey)
+			return nil, "", err
+		}
+		return wallet, string(clientBytes), nil
+	}
+	var walletFilePath string
+	if filepath.IsAbs(walletFile) {
+		walletFilePath = walletFile
+	} else {
+		configDir, err := GetConfigDir()
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, "", err
+		}
+		walletFilePath = configDir + string(os.PathSeparator) + walletFile
+	}
+
+	var walletStr string
+	if _, err := os.Stat(walletFilePath); os.IsNotExist(err) {
+		walletStr, err = zcncore.CreateWalletOffline()
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, "", err
+		}
+		fmt.Println("ZCN wallet created")
+		file, err := os.Create(walletFilePath)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, "", err
+		}
+		defer file.Close()
+		fmt.Fprint(file, walletStr)
+	} else {
+		f, err := os.Open(walletFilePath)
+		if err != nil {
+			fmt.Println("Error opening the wallet", err)
+			return nil, "", err
+		}
+		clientBytes, err := io.ReadAll(f)
+		if err != nil {
+			fmt.Println("Error reading the wallet", err)
+			return nil, "", err
+		}
+		walletStr = string(clientBytes)
+	}
+	err := json.Unmarshal([]byte(walletStr), wallet)
+	if err != nil {
+		fmt.Println("Invalid wallet at path:" + walletFilePath)
+		return nil, "", err
+	}
+	return wallet, walletStr, nil
+}
+
+// creates config file "config.yaml" with default configuration in user's configuration directory.
+func createConfigFile() error {
+	appConfigDir, err := GetAppConfigDir()
+	if err != nil {
+		fmt.Println("error getting appConfigDir :", err)
+		return err
+	}
+	if err := os.MkdirAll(appConfigDir, 0744); err != nil {
+		fmt.Println("error creating default configuration directory :", err)
+		return err
+	}
+	// fmt.Printf("created default configuration directory : %v", appConfigDir)
+	configFile := appConfigDir + string(os.PathSeparator) + "config.yaml"
+
+	if _, err = os.Stat(configFile); os.IsNotExist(err) {
+		file, err := os.Create(configFile)
+		if err != nil {
+			fmt.Println("error creating configFile :", err)
+			return err
+		}
+		defer file.Close()
+		_, err = fmt.Fprint(file, configStr)
+		if err != nil {
+			fmt.Println("error writing default configuration :", err)
+			return err
+		}
+		fmt.Printf("created default configuration file : %v\n", configFile)
+	}
+	return nil
+}
+
+func initConfig() {
+
+	// DEBUG
+	appConfigDir, _ := GetAppConfigDir()
+	fmt.Printf("DEBUG: appConfigDir = %v\n", appConfigDir)
+
+	_ = createConfigFile()
+
+	// load config
+	var err error
+	cfg, err = LoadConfig()
+	if err != nil {
+		fmt.Printf("error loading config: %v", err)
+		os.Exit(1)
+	}
+
+	// load network file
+	network, _ = LoadNetworkFile()
+
+	// init gosdk
+	err = initSDK()
+	if err != nil {
+		fmt.Println("error initializing SDK: ", err)
+		os.Exit(1)
+	}
+
+	// load wallet info
+	clientWallet, walletJSON, err = loadWallet()
+	if err != nil {
+		fmt.Println("error loading wallet: ", err)
+		os.Exit(1)
+	}
+
+	//init the storage sdk with the known miners, sharders and client wallet info
+	if err = sdk.InitStorageSDK(
+		walletJSON,
+		cfg.BlockWorker,
+		cfg.ChainID,
+		cfg.SignatureScheme,
+		cfg.PreferredBlobbers,
+		nonce,
+		zcncore.ConvertToValue(txFee),
+	); err != nil {
+		fmt.Println("Error in sdk init", err)
+		os.Exit(1)
+	}
+
+	// set wallet info along whether split key is used
+	err = zcncore.SetWalletInfo(walletJSON, false)
+	if err != nil {
+		fmt.Println("Error in wallet info initialization", err)
+		os.Exit(1)
+	}
+
+	// additional settings depending network latency
+	blockchain.SetMaxTxnQuery(cfg.MaxTxnQuery)
+	blockchain.SetQuerySleepTime(cfg.QuerySleepTime)
+
+	conf.InitClientConfig(&cfg)
+
+	if network.IsValid() {
+		sdk.SetNetwork(network.Miners, network.Sharders)
+	}
+
+	sdk.SetNumBlockDownloads(10)
 }
