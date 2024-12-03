788469e4dfbd0e507040646d8925a8036545fb86
Arun Ramanathan <arunmail12@gmail.com>
2024-12-03 07:08:48 +0530

    Fix issues 2 (#14)
    * fix duplicate fee flag
    
    * set log flags

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/0chain/zus-cli/cmd"
+import (
+	"log"
+	"os"
+
+	"github.com/0chain/zus-cli/cmd"
+)
 
 var VersionStr string
 
 func main() {
 	cmd.VersionStr = VersionStr
+	log.SetOutput(os.Stdout)
+	log.SetFlags(0)
 	cmd.Execute()
 }
